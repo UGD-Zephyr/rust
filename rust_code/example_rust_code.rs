1ae437d3f16d827c227e738a692029a5c19d34ac
Per Stoor <per.stoor@gmail.com>
2024-01-28 14:06:38 +0100

    Uploading rust code

---
+++ rust_code/example_rust_code.rs
@@ -0,0 +1,10 @@
+/* Programmer:          Per Stoor
+ * Date:                2024-01-28
+ * Last changed:        2024-01-28
+ * Type of program:     
+ */
+
+fn main(){ 
+
+    println!("I only exist to make the Github repo look nice!");
+} 
